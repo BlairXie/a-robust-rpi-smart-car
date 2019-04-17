2117bf04c7a7c49d2bf487d629ae9a573874206a
BlairXie <1263815597@qq.com>
2019-04-17 11:57:01 +0800

    comment

--- motor.py
+++ motor.py
@@ -11,8 +11,8 @@ GPIO_IN3 = 18
 GPIO_IN4 = 23
 ENA = 7
 ENB = 8
-run_flag = 1 
-pre_run_flag =0
+run_flag = 1 #当前运动状态标志
+pre_run_flag =0 #上一次运动状态标志
 
 GPIO.setwarnings(False)
 #设置 GPIO 的工作方式 (IN / OUT)
@@ -59,6 +59,8 @@ def pause():
     GPIO.output(GPIO_IN4,True)
 
 def forward_avoid_obstacle():
+    '''前进并避障函数
+    遇到障碍物时先后退再左转'''
     dist = distance()
     print("Measured Distance = {:.2f} cm".format(dist))
     if(dist < 25 ):
@@ -79,6 +81,9 @@ def forward_avoid_obstacle():
 
 
 def motion_ctrol(child_pipe):
+    '''方向控制函数
+    如果改变转动方向，则转动一定角度后又继续直走并且避障
+    '''
     global run_flag
     global pre_run_flag 
     pause()
@@ -100,6 +105,7 @@ def motion_ctrol(child_pipe):
             #    run_flag = share_state.value
             #    print("signal accepted")
             #    share_state.value = -1
+            #如果之前暂停现在仍然暂停就跳过本次循环
             if pre_run_flag == 0 and pre_run_flag == run_flag:
                continue 
             if run_flag == 0:
@@ -123,10 +129,10 @@ def motion_ctrol(child_pipe):
                 time.sleep(1)
                 run_flag = 1
             pre_run_flag = run_flag
-            # exit by pressing CTRL + C
+    # exit by pressing CTRL + C
     except KeyboardInterrupt:
-        p1.stop()
+        p1.stop()#使能端PWM波停止，电机停止
         p2.stop()
         print("Measurement stopped by User")
-        GPIO.cleanup()
+        GPIO.cleanup()#清空GPIO数据
         sys.exit(0)
